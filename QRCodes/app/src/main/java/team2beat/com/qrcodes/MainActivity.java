4e6e5f609dde0b3d000f2a056df877b1dba0699d
Auctioneer <ljgdavie@hotmail.com>
2016-02-19 14:41:49 +0000

    Removed floating action button in app.

--- QRCodes/app/src/main/java/team2beat/com/qrcodes/MainActivity.java
+++ QRCodes/app/src/main/java/team2beat/com/qrcodes/MainActivity.java
@@ -27,15 +27,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 
@@ -78,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
 
                 try {
-                    
+
                     activity.startActivity(intent);
 
                 } catch (ActivityNotFoundException e) {
