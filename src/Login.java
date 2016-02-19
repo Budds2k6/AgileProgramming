6c9b4a02c3aa00667563c094f9c3e7b54ad5ac4d
mmalone136 <mmalone136@gmail.com>
2016-02-19 14:33:52 +0000

    Login class added

---
+++ src/Login.java
@@ -0,0 +1,17 @@
+
+public class Login {
+Login(){}
+
+
+
+public void doLogin()
+{	
+
+}
+
+public boolean checkValid(String username, String password)
+{	
+	return true;
+}
+
+}
--- src/User.java
+++ src/User.java
@@ -5,4 +5,6 @@ String userID;
 String password;
 String firstname;
 String surname;
+
+
 }
