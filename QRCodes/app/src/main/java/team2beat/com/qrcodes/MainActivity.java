862fc678612830228bdf749702ac9195414dd0a6
Andrew Davis <asutherlandavis@googlemail.com>
2016-02-22 13:07:21 +0000

    QR Code generation boiz

--- QRCodes/app/src/main/java/team2beat/com/qrcodes/MainActivity.java
+++ QRCodes/app/src/main/java/team2beat/com/qrcodes/MainActivity.java
@@ -8,20 +8,45 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import com.google.zxing.qrcode.encoder.QRCode;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+
+import java.text.SimpleDateFormat;
+
 import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     static final String SCAN = "com.google.zxing.client.android.SCAN";
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +54,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 
-    public void ScanQR(View v)
-    {
-        try
-        {
+    public void ScanQR(View v) {
+        try {
             // Create a new 'Intent to scan'
             Intent intent = new Intent(SCAN);
             // Set up a variable called 'Scan Mode' which tells us which type of scan it will be doing (QR)
@@ -45,16 +71,14 @@ public class MainActivity extends AppCompatActivity {
             startActivityForResult(intent, 0);
 
             // If the activity does not exist - i.e. no scanner found
-        }catch (ActivityNotFoundException e)
-        {
+        } catch (ActivityNotFoundException e) {
 
             showDialog(MainActivity.this, "No Scanner Found", "Download a scanner code activity?", "Yes", "No").show();
 
         }
     }
 
-    private Dialog showDialog(final Activity activity, CharSequence title, CharSequence message, CharSequence Yes, CharSequence No)
-    {
+    private Dialog showDialog(final Activity activity, CharSequence title, CharSequence message, CharSequence Yes, CharSequence No) {
 
         AlertDialog.Builder download = new AlertDialog.Builder(activity);
 
@@ -81,11 +105,10 @@ public class MainActivity extends AppCompatActivity {
         });
 
         download.setNegativeButton(No, new DialogInterface.OnClickListener() {
-           @Override
-           public void onClick(DialogInterface dialog, int intf)
-           {
+            @Override
+            public void onClick(DialogInterface dialog, int intf) {
 
-           }
+            }
 
         });
 
@@ -94,11 +117,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent)
-    {
-        if(requestCode == 0) {
-            if(resultCode == RESULT_OK)
-            {
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
                 // Is called when activity exists. Gives the result it found - with any additional data
                 String contents = intent.getStringExtra("SCAN_RESULT");
 
@@ -112,29 +133,112 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void generateQRCode()
-    {
-        // The value to be represented by the QR Code
-        String scanValue = "HI! I am Andrew!!!";
+    public void generateQRCode(View v) {
+        String content = "Hi Rachel, This is a code";
+        QRCodeWriter write = new QRCodeWriter();
+        try {
+            int width = 125;
+            int height = 125;
+            BitMatrix bitMatrix = write.encode(content, BarcodeFormat.QR_CODE, width, height);
+            Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            for (int i = 0; i < width; i++) {
+                for (int j = 0; j < height; j++) {
+                    bitmap.setPixel(i, j, bitMatrix.get(i, j) ? Color.BLACK : Color.WHITE);
+                }
+            }
 
-        // File Path of where to save the image
-        String filePath = "tmpFile.jpg";
+            saveBitmap(bitmap);
 
-        int size = 125;
-        String fileType = ".jpg";
+            ImageView image = (ImageView)findViewById(R.id.qrImage);
+            image.setImageBitmap(bitmap);
 
-        File myFile = new File(filePath);
 
-        try
-        {
-            // Generate the image and store in the specified location
-
-        }catch(Exception e)
-        {
-
+            //mImageView.setImageBitmap(bitmap);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-
     }
 
+    private void saveBitmap(Bitmap image) {
+        File pictureFile = getOutputMediaFile();
+        if (pictureFile == null) {
+            //Log.d(TAG,
+            //"Error creating media file, check storage permissions: ");// e.getMessage());
+            Log.d("TEST", "BROKEN A LOT");
+            return;
+        }
+        try {
+            FileOutputStream fos = new FileOutputStream(pictureFile);
+            image.compress(Bitmap.CompressFormat.PNG, 90, fos);
+            fos.close();
+            Log.d("TEST", "WORKED");
+        } catch (Exception e) {
+            Log.d("TEST", "BROKEN");
+        }
+    }
+
+    private File getOutputMediaFile() {
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+        File mediaStorageDir = new File(Environment.getExternalStorageDirectory()
+                + "/Android/data/"
+                + getApplicationContext().getPackageName()
+                + "/Files");
+
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                return null;
+            }
+        }
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("ddMMyyyy_HHmm").format(new Date());
+        File mediaFile;
+        String mImageName = "MI_" + timeStamp + ".jpg";
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator + mImageName);
+        return mediaFile;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://team2beat.com.qrcodes/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://team2beat.com.qrcodes/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
