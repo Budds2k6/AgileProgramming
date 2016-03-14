01a4610e49e5161d9c1db88127dfce501a5e1bc7
mmalone136 <mmalone136@gmail.com>
2016-03-14 12:44:27 +0000

    Fixed broken connection check. Fixed broken link between pages. Fixed broken output

--- QRCodes/app/src/main/java/team2beat/com/src/AsyncClasses/ConnectionCheck.java
+++ QRCodes/app/src/main/java/team2beat/com/src/AsyncClasses/ConnectionCheck.java
@@ -18,12 +18,38 @@ public class ConnectionCheck implements Runnable
     {
         try{
             // try and get an address for the web API file path
-            InetAddress ipAddr = InetAddress.getByName("http://silva.computing.dundee.ac.uk/2015-agileteam2/");
+            //InetAddress ipAddr = InetAddress.getByName("http://silva.computing.dundee.ac.uk/2015-agileteam2/Login");
+
+/*
+            InetAddress ipAddr = InetAddress.getByAddress("http://www.google.co.uk");
+
+
+            InetAddress ipA = InetAddress.getBy
+
 
             // if there is an address, the connection is available
             if (!ipAddr.equals("")) {
                 success = true;
-            }
+            }*/
+
+//            InetSocketAddress isa = new InetSocketAddress("http://www.google.co.uk", 3306);
+//            Socket sock = new Socket();
+//            sock.connect(isa,50);
+//            sock.close();
+//            success = true;
+
+
+
+            //boolean seven = InetAddress.getByName("http://www.google.com").isReachable(50);
+
+            URL myUrl = new URL("http://silva.computing.dundee.ac.uk/2015-agileteam2");
+            URLConnection connection = myUrl.openConnection();
+            connection.setConnectTimeout(25);
+            connection.connect();
+
+            System.out.println("Placeholder");
+
+            success = true;
 
             // catch exceptions
         } catch (Exception e) {
